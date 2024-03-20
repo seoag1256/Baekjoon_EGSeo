f7c87028bf4704ecbac8b8a45ca7c45fc6894d2b
Eungyo <72914431+seoag1256@users.noreply.github.com>
2024-03-21 00:45:11 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int score;
+	char grade;
+
+	cin >> score;
+
+	if (score >= 90)
+		grade = 'A';
+	else if (score >= 80)
+		grade = 'B';
+	else if (score >= 70)
+		grade = 'C';
+	else if (score >= 60)
+		grade = 'D';
+	else
+		grade = 'F';
+
+	cout << grade << endl;
+}
