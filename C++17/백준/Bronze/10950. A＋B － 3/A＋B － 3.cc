8dbc522d6a899e99118a5bee0261ecc3b68f4b66
Eungyo <72914431+seoag1256@users.noreply.github.com>
2024-03-29 00:40:13 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int A;
+	int B;
+	int num;
+	cin >> num;
+
+	for (int i = 0; i < num; i++) {
+		cin >> A >> B;
+		cout << A + B << endl;
+	}
+
+}
