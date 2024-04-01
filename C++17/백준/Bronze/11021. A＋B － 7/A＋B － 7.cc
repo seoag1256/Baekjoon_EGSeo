981e41709317a2c98a65c554fe0b8833757da517
Eungyo <72914431+seoag1256@users.noreply.github.com>
2024-04-01 22:46:29 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int count;
+	int num1, num2;
+
+	cin >> count;
+
+	for (int i = 0; i < count; i++) {
+		cin >> num1 >> num2;
+		cout << "Case #" << (i + 1) << "\: " << (num1 + num2) << endl;
+	}
+
+}
