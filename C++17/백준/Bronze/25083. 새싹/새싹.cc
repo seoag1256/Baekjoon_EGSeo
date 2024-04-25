8c87cef8df0ca6c99ed032999a46b07f58492c74
Eungyo <72914431+seoag1256@users.noreply.github.com>
2024-04-26 00:18:25 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str1 = "         ,r'\"7";
+	string str2 = "r`-_   ,'  ,/";
+	string str3 = " \\. \". L_r\'";
+	string str4 = "   `~\\/";
+	string str5 = "      |";
+	string str6 = "      |";
+
+	cout << str1 << "\n";
+	cout << str2 << "\n";
+	cout << str3 << "\n";
+	cout << str4 << "\n";
+	cout << str5 << "\n";
+	cout << str6 << "\n";
+}
